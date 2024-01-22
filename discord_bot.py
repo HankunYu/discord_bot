26016715132af711fa119f323b6a13619391b1bb
HankunYu <KmcyD1@gmail.com>
2024-01-22 22:21:19 +0000

    添加 使用斜杠命令控制

--- discord_bot.py
+++ discord_bot.py
@@ -1,91 +1,38 @@
-import discord
-import gpt
-import asyncio
-import re
-import discord.webhook
+import discord, gpt, re, discord.webhook, asyncio, cogs.moviepilot_cog as moviepilot_cog, os
+from discord import app_commands
 from discord.ui import Button, View
 from discord.ext import commands
-from shared_variables import current_channel, discord_token, converstation_timer
+from shared_variables import current_channel, discord_token
 
 """
 bot 本体.
-只回应被@的消息,或者在对话中.
-当对话超过5分钟没有回应,则bot会自动结束对话.
 """
 
-intents = discord.Intents.default()
-intents.message_content = True
 on_conversion = False
-timer = converstation_timer
 current_channel = None
-intents = discord.Intents.default()
-intents.message_content = True
-client = commands.Bot(command_prefix='!', intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-    asyncio.create_task(countdown())
-
-@client.event
-async def on_message(message):
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='$', intents=intents)
     
-    global on_conversion
-    global timer
-    global current_channel
+@client.command(name="sync")
+async def sync(ctx):
+    slash = await client.tree.sync()
+    await ctx.send(f"載入 {len(slash)} 個斜線指令")
 
-    if message.author == client.user:
-        return
+   
 
-    if client.user.mentioned_in(message) or on_conversion:
-        print('get mentioned ', timer)
-        msg  = re.sub(r'<.*?>', '', message.content)
-        timer = converstation_timer  # 设置5分钟的倒计时
-        on_conversion = True
-        current_channel = message.channel
-        reply = gpt.define_command(msg)
-        print('reply: ', reply)
-        if reply != None:
-            await message.channel.send(reply)
-
-@client.command()
-async def button_example(ctx):
-    # 创建按钮
-    button = Button(label="点击我", style=discord.ButtonStyle.primary, custom_id="my_button")
-
-    # 创建视图
-    view = View()
-    view.add_item(button)
-
-    # 发送消息并附上视图
-    await ctx.send("这是一个按钮示例", view=view)
-
-@client.event
-async def on_button_click(interaction: discord.Interaction):
-    if interaction.component.custom_id == "my_button":
-        print("按钮被点击111！")
-        await my_button_callback(interaction)
-
-async def my_button_callback(interaction: discord.Interaction):
-    await interaction.response.send_message("按钮被点击！")
-
-
-async def countdown():
-    global on_conversion
-    global timer
-
-    while True:
-        if on_conversion:
-            if timer > 0:
-                timer -= 1
-            else:
-                on_conversion = False
-                gpt.clear_chat_history()
-                await send_message('我洗澡去了,有事请@我')
-        await asyncio.sleep(1)
+# Load cogs
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"cogs.{filename[:-3]}")
 
 async def send_message(reply, embed=None, view=None):
     await current_channel.send(reply, embed=embed, view=view)
 
-def run_bot():
-    client.run(discord_token)
+async def main():
+    async with client:
+        await load_extensions()
+        await client.start(discord_token)
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- gpt.py
+++ gpt.py
@@ -1,9 +1,7 @@
 import openai
-import json
-import webhook
-import discord_bot
+import shared_variables
 
-openai.api_key = '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'
+openai.api_key = shared_variables.gpt_token
 chat_history = [
             {"role": "system", "content": "你是一个智能助手,请用中文回复我"}
         ]
@@ -14,26 +12,6 @@ chat_start = [
 def clear_chat_history():
     chat_history = chat_start
 
-# 识别用户的意图
-def define_command(message):
-    propose = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "分析我发你的内容,当识别到我有下载/订阅/搜索电影或者电视剧的意向时候回复action,其余情况回复chat"},
-            {"role": "user", "content": "我想看霸王别姬"},
-            {"role": "assistant", "content": "action"},
-            {"role": "user", "content": "我好无聊"},
-            {"role": "assistant", "content": "chat"},
-            {"role": "user", "content": "你好"},
-            {"role": "assistant", "content": "chat"},
-            {"role": "user", "content": message}
-        ]
-        )
-    answer = propose.choices[0].message.get('content')
-    if(answer == 'action'):
-        return run_command(message)
-    else:
-        return generate_reply(message)
     
 # 生成回复,并添加到chat_history
 def generate_reply(message):
@@ -49,22 +27,3 @@ def generate_reply(message):
     chat_history.append({"role": "assistant", "content": chat.choices[0].message.get('content')})
 
     return chat.choices[0].message.get('content')
-
-# 识别为命令则执行webhook
-def run_command(command):
-        completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "请帮我分析我发你的内容,如果可以识别就以json格式回复我: '{'action: title: '}',action的行为只有subscribe,download,search, 如果不能识别请回复我抱歉并询问详细内容,电影两个字不能作为title"},
-            {"role": "user", "content": command}
-        ]
-        )
-        reply = completion.choices[0].message.get('content')
-        print(reply)
-        if reply.startswith('抱歉'):
-            return reply
-        try:
-            reply = json.loads(reply)
-        except:
-            return reply + ' 不是json'
-        webhook.send_message(reply.get('title'), reply.get('action'))
